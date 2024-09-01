13ccc5ab26dd2ea579efcb8a02315e755bfe4d30
AnaBMamprin <ana.b.mamprin@gmail.com>
2024-09-01 00:45:46 -0300

    first commit

---
+++ app/src/main/java/br/edu/fatec/poupanca/MainActivity.java
@@ -0,0 +1,33 @@
+package br.edu.fatec.poupanca;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCalcular;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnCalcular = findViewById(R.id.btnCalcular);
+    }
+
+        public void calcular(View view){
+            Toast.makeText(this, "Não tem nada programado aqui", Toast.LENGTH_LONG).show();
+        }
+}
